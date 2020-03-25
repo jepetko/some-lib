6f42e09c3fa341e07057747f8999469b39980939
Katarina Golbang <katarina.golbang@rbinternational.com>
2020-03-25 14:29:15 +0100

    some-lib provides A, B for dependency injection, make it more reasonable

--- src/lib/b.ts
+++ src/lib/b.ts
@@ -4,6 +4,7 @@ import {A} from './a';
 @Singleton
 export class B {
     constructor(@Inject private a: A) {
-        console.info('B created: ', this.a.method());
+        console.info('B created, will now call this.a.method()');
+        this.a.method();
     }
 }
